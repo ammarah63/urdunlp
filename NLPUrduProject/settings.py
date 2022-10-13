58ecbd9420653cfa714328473f8df63fa00aec07
ammarah63 <ammarahkhaleeq@gmail.com>
2022-10-13 02:59:41 -0700

    18th commit

--- NLPUrduProject/settings.py
+++ NLPUrduProject/settings.py
@@ -133,10 +133,8 @@ REST_FRAMEWORK = {
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 
-STATIC_ROOT = [
-     os.path.join(BASE_DIR, 'frontend/build/static')
-]
 
 
 # Default primary key field type
