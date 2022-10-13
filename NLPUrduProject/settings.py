f9ea6e15e8e05dec856d4431ba1caa04047d7ae3
ammarah63 <ammarahkhaleeq@gmail.com>
2022-10-13 02:54:32 -0700

    17th commit

--- NLPUrduProject/settings.py
+++ NLPUrduProject/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
+STATIC_ROOT = [
      os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 
