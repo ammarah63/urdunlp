4592eca5fca30c64ccbefc4196f249b630e9d631
ammarah63 <ammarahkhaleeq@gmail.com>
2022-10-13 23:26:34 -0700

    29th commit

--- NLPUrduProject/settings.py
+++ NLPUrduProject/settings.py
@@ -133,7 +133,8 @@ REST_FRAMEWORK = {
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
