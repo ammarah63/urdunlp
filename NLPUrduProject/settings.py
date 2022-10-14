b7d63d47c7543e85416b1dd2ee1f66f685fa7d7d
ammarah63 <ammarahkhaleeq@gmail.com>
2022-10-14 00:41:14 -0700

    31st commit

--- NLPUrduProject/settings.py
+++ NLPUrduProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ag0!b50ixu$f2r*0k1gp=q(#19-99=@22#lpm==m5rt%kt7y@="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
