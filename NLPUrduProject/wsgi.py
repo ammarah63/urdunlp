46702ca6b7ef63c72d04daddae56973914718e1d
ammarah63 <ammarahkhaleeq@gmail.com>
2022-10-17 22:54:34 -0700

    31st commit

--- NLPUrduProject/wsgi.py
+++ NLPUrduProject/wsgi.py
@@ -11,8 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
+
+from NLPUrduProject.settings import BASE_DIR
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NLPUrduProject.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+BASE_DIR =Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=os.path.join(BASE_DIR,'build/static'))
+
+
+
